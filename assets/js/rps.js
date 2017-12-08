4107ce4bcb7cd3f7e4b2e29b575a953f2f480e47
Victoria <victoria@some-mail.com>
2017-12-08 00:29:15 -0800

    multiple games added for each pair of players. todo: disconnect functionality

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -14,8 +14,8 @@ var db = firebase.database();
 
 var room_id = 0;
 
-var opponent_id = 0;
-var opponent_choice = 0;
+// var opponent_id = 0;
+// var opponent_choice = 0;
 
 var player = {
   id : 0, 
@@ -24,6 +24,15 @@ var player = {
   looses : 0,
   choice : 0
 }
+
+var opponent = {
+  id : 0, 
+  name : "",
+  wins : 0,
+  looses : 0,
+  choice : 0  
+};
+
 //var player_turn = 0;
 var game_started = false;
 var game_wait_time_start = null;
@@ -31,10 +40,15 @@ var game_wait_time_start = null;
 //=================================== 
 
 function drawMyChoiceButtons(){
-  var myName = $("<span>");
-  myName.text(player.name);
-  $("#player1").append(myName);
-//draw r p s
+
+  $("#player1").empty();
+  $("#player2").empty();
+  $("#game-result").empty();
+  // var myName = $("<span>");
+  // myName.text(player.name);
+  // $("#player1").append(myName);
+
+  //draw r p s
   var rock_btn = $("<button>");
   rock_btn.addClass("choice_btn");
   rock_btn.attr("data", "r");
@@ -55,6 +69,10 @@ function drawMyChoiceButtons(){
 
 }
 
+function drawBoxes(){
+
+}
+
 function getTextForChoice( choice ){
   switch(choice){
     case 'r' : return "=Rock=";
@@ -65,60 +83,69 @@ function getTextForChoice( choice ){
 
 $("#player1").on("click", ".choice_btn", function(){
 
-  var p1c = $(this).attr("data");
-  var p2c = 0;
-  console.log("p1-choice: "+p1c);
-  //draw/show a picture of chice 
+  player.choice = $(this).attr("data");
+  console.log("p1-choice: "+player.choice);
+  //draw/show a picture of choice 
   $("#player1").empty();
-  var p1text = getTextForChoice( p1c );
-  //draw/show pic for opponent choice
+  var p1text = getTextForChoice( player.choice );
   $("#player1").text(p1text);
-
   //save choice to db
-  db.ref(room_id+'/players/'+player.id).update({ choice: p1c });
+  db.ref(room_id+'/players/'+player.id).update({ choice: player.choice });
 
-//  db.ref(room_id+'/players/'+opponent_id+'/choice').on('value', function(snapshot){
-  db.ref(room_id+'/players/'+opponent_id).on('value', function(snapshot){
-    console.log("opponent : "+opponent_id);
-    var p2 = snapshot.val();
-    console.log("opponents choice changed: "+ p2);
-    if( p2 ){
-      p2c = p2.choice;
-      console.log("p2-choice: "+p2c);
-      if(p2c){
-        opponent_choice = p2c;
-        // console.log("p2-choice: "+p2c);
-        var p2text = getTextForChoice( p2c )
-        //draw/show pic for opponent choice
-        $("#player2").text(p2text);
+  opponent.choice = 0;
 
-        //check who is the winner + timeout 5 sec?
-        if (p1c == p2c) {
-          $("#game-result").text("Ties!");
-        }
-        else if( (p1c == "r" && p2c == "s") ||
-            (p1c == "p" && p2c == "r") ||
-            (p1c == "s" && p2c == "p")){
-            $("#game-result").text("You Win!");
-            console.log("WIN");
-        }
-        else{
-          $("#game-result").text("You Loose!");
-          console.log("LOOSE");
-        } 
+  db.ref(room_id+'/players/'+opponent.id+'/choice').on('value', function(snapshot){
+  //db.ref(room_id+'/players/'+opponent.id).on('value', function(snapshot){
+    console.log("opponent : "+opponent.id);
+    opponent.choice = snapshot.val();
+    console.log("opponents choice: "+ opponent.choice);
 
-        opponent_choice = 0;
-        player.choice = 0;
-        //set self coice to 0 in db
-        //increments wins or looses in obj and db
+    if( opponent.choice && player.choice ){
+
+      console.log("p2-choice: "+ opponent.choice);
+      //if( player.choice ){
+      var p2text = getTextForChoice( opponent.choice );
+      //draw/show pic for opponent choice
+      $("#player2").text(p2text);
+
+      //check who is the winner
+      if (player.choice == opponent.choice) {
+        $("#game-result").text("Ties!");
       }
+      else if( (player.choice == "r" && opponent.choice == "s") ||
+        (player.choice == "p" && opponent.choice == "r") ||
+        (player.choice == "s" && opponent.choice == "p")){
+          $("#game-result").text("You Win!");
+          player.wins++;
+          opponent.looses++;
+          console.log("WIN");
+      }
+      else{
+        $("#game-result").text("You Loose!");
+        player.looses++;
+        opponent.wins++;
+
+        console.log("LOOSE");
+      } 
+
+      opponent.choice = 0;
+      player.choice = 0;
+
+      //set self coice to 0 in db
+      db.ref(room_id+'/players/'+player.id+'/choice').set(0);
+      db.ref(room_id+'/players/'+player.id+'/wins').set(player.wins);
+      db.ref(room_id+'/players/'+player.id+'/looses').set(player.looses);
+
+      //redraw buttons in 5 sec?
+      setTimeout( drawMyChoiceButtons , 10000 );
+      
     }
   }, 
     function(error){
       console.log(error.code);
   });
 
-});
+});//choice click
 
 
 $("#btn-start").on("click", function(){
@@ -133,7 +160,6 @@ $("#btn-start").on("click", function(){
 
   $("#start-form").hide();
 
-  //to do: draw greetings, name, game status etc.
 
   db.ref('waiting').once('value', function(snapshot) {
     var roomW = snapshot.val();
@@ -151,7 +177,7 @@ $("#btn-start").on("click", function(){
         console.log(pl_id, player.id);
         if(pl_id != player.id && roomW[pl_id]==0 ){
           //generate room_id
-          opponent_id = pl_id;
+          opponent.id = pl_id;
           room_id = "room_"+Math.ceil(Math.random()*10000);
           break;
         }
@@ -168,7 +194,7 @@ $("#btn-start").on("click", function(){
         db.ref().update(tmp_r);
 
         //add room_id to opponent's id
-        db.ref('waiting/' + opponent_id).set(room_id);
+        db.ref('waiting/' + opponent.id).set(room_id);
 
         //add yourself to waiting with the same room_id
         var tmp_p = {};
@@ -196,13 +222,11 @@ $("#btn-start").on("click", function(){
     console.log("error: "+error.code);
   });
 
-  console.log("0000000");
-
   //listener for room_id changes for self-id in waiting room
   db.ref('waiting/'+player.id).on('value', function(snapshot) {
 
       //get opponents id, add yourself to the room
-      console.log("my status in waiting list changed : "+snapshot.val());
+      console.log("waiting list changed : "+snapshot.val());
 
       if(snapshot.val()){
 
@@ -216,13 +240,13 @@ $("#btn-start").on("click", function(){
           if(snap.val()){
             for(var p_id in snap.val()){
               if(p_id != player.id ){
-                opponent_id = p_id;
-                opponent_choice = snap.val()[p_id].coice;
+                opponent.id = p_id;
+                opponent.choice = snap.val()[p_id].choice;
                 break;
               }
             }
-            console.log("opponent: "+ opponent_id);
-            console.log("opponent coice: "+ opponent_choice);
+            console.log("opponent: "+ opponent.id);
+            console.log("opponent choice: "+ opponent.choice);
           }
         });
 

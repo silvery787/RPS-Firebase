2419498b3de4256856f489fede76984ba4f17fe8
Victoria <victoria@some-mail.com>
2017-12-09 18:22:15 -0800

    finished disconnect processing and pair matching

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -19,8 +19,7 @@ var player = {
   name : "",
   wins : 0,
   looses : 0,
-  choice : 0,
-  status : "online"
+  choice : 0
 }
 
 var opponent = {
@@ -28,47 +27,52 @@ var opponent = {
   name : "",
   wins : 0,
   looses : 0,
-  choice : 0,
-  status : null
+  choice : 0
 };
 
 //=================================== 
 function clearButtons(){
+
   $("#player1").empty();
   $("#player2").empty();
   $("#game-result").empty(); 
   
-  $("#p2name").text("disconnected");
-  $("#p2score").text();
+  $("#p2name").text('disconnected');
+  $("#p2score").empty();
+
 }
 
 function drawMyChoiceButtons(){
   
-  drawPlayersInfo();
+  if(player.id && opponent.id){
+    drawPlayersInfo();
 
-  $("#player1").empty();
-  $("#player2").empty();
-  $("#game-result").empty();
+    $("#player1").empty();
+    $("#player2").empty();
+    $("#game-result").empty();
 
-  //draw r p s
-  var rock_btn = $("<button>");
-  rock_btn.addClass("choice_btn");
-  rock_btn.attr("data", "r");
-  rock_btn.text("=Rock=");
-  $("#player1").append(rock_btn);
+    //draw r p s
+    var rock_btn = $("<button>");
+    rock_btn.addClass("choice_btn");
+    rock_btn.attr("data", "r");
+    rock_btn.text("=Rock=");
+    $("#player1").append(rock_btn);
 
-  var paper_btn = $("<button>");
-  paper_btn.addClass("choice_btn");
-  paper_btn.attr("data", "p");
-  paper_btn.text("=Paper=");
-  $("#player1").append(paper_btn);
+    var paper_btn = $("<button>");
+    paper_btn.addClass("choice_btn");
+    paper_btn.attr("data", "p");
+    paper_btn.text("=Paper=");
+    $("#player1").append(paper_btn);
 
-  var scissors_btn = $("<button>");
-  scissors_btn.addClass("choice_btn");
-  scissors_btn.attr("data", "s");
-  scissors_btn.text("=Scissors=");
-  $("#player1").append(scissors_btn);
-
+    var scissors_btn = $("<button>");
+    scissors_btn.addClass("choice_btn");
+    scissors_btn.attr("data", "s");
+    scissors_btn.text("=Scissors=");
+    $("#player1").append(scissors_btn);
+  }
+  else {
+    clearButtons();
+  }
 }
 
 function drawPlayersInfo(){
@@ -91,68 +95,108 @@ function getTextForChoice( choice ){
 
 function enterWaitList(){
 
-  db.ref('waiting').once('value', function(snapshot) {
-    var roomW = snapshot.val();
-    console.log(roomW);
+//  db.ref('waiting').once('value', function(snapshot) {
+  var query = db.ref('waiting').orderByKey();
+  query.once('value').then( function(snapshot) {
+    //var roomW = snapshot.val();
+    //console.log(roomW);
     //if doesn't exist
-    if( !roomW ){
-      console.log("whaiting room: null");
-      var tmp_p = {};
-      tmp_p[player.id] = 0;
-      db.ref('waiting').set(tmp_p);
+    if( !snapshot.exists()){
+      db.ref('waiting/'+player.id).set(0);
     }
     else {
       //waiting room exists
-      for( var pl_id in roomW ){
-        console.log(pl_id, player.id);
-        if(pl_id != player.id && roomW[pl_id]==0 ){
-          //other waiting player found -> generate room_id
-          opponent.id = pl_id;
+      //chack if there any any waiting player
+      snapshot.forEach(function(childSnapshot) {
+        var id = childSnapshot.key;
+        var val = childSnapshot.val();
+        if( id != player.id && val == 0 ){
+          opponent.id = id;
           room_id = "room_"+Math.ceil(Math.random()*10000);
-          break;
+          return false;
         }
-      }
-      console.log(room_id);
-      if(room_id!=0){// if opponent was found
+        if(room_id) return false;
+      });
+
+      console.log("Room = "+room_id);
+
+      if( room_id != 0 ){// if opponent was found
         //create new room
-        var tmp_r = {};
-        tmp_r[room_id] = {
-          players : 0,
+        db.ref(room_id).set({
+          players: 0,
           chat : ""
-        };
-        db.ref().update(tmp_r);
-
-        //add room_id to opponent's id
-        db.ref('waiting/' + opponent.id).set(room_id);
-
-        //add yourself to waiting with the same room_id
-        var tmp_p = {};
-        tmp_p[player.id] = room_id;
-        db.ref('waiting').update(tmp_p);
+        });
 
         //add yourself to the new room
-        tmp_p = {};
-        tmp_p[player.id] = player;
-        db.ref(room_id+'/players').update(tmp_p);
+        //db.ref(room_id+'/players/'+player.id).set(player);
+
+        //add room_id to opponent's id in waiting room
+        db.ref('waiting/' + opponent.id).set(room_id);
+        //add yourself to waiting with the same room_id
+        db.ref('waiting/'+player.id).set(room_id);
 
       }
       else{
         //opponent.id is undefined yet
         //add yourself to waiting room with room value == 0
-        var tmp_p = {};
-        tmp_p[player.id] = 0;
-        //db.ref('waiting').update(tmp_p);
-        db.ref('waiting').update(tmp_p);
+        db.ref('waiting/'+player.id).set(0);
 
       }
-      
+      query.off(); 
     }
   },
   function(error){
     console.log("error: "+error.code);
   });
+  
 }
-//=========================
+
+function processOpponentDisconnect(){
+  
+  db.ref(room_id+'/players/'+opponent.id).once('child_removed').then( function(event){
+    
+    db.ref(room_id).remove();
+    player.choice = 0;
+    opponent = {};
+    room_id = 0;
+    
+    clearButtons();
+
+    enterWaitList();
+  //   var query = db.ref("waiting").orderByKey();
+  //   query.once("value").then(function(parentSnapshot) {
+  //     parentSnapshot.forEach(function(childSnapshot) {
+  //       var id = childSnapshot.key;
+  //       var val = childSnapshot.val();
+  //       if( id != player.id && val == 0 ){
+  //         opponent.id = id;
+  //         room_id = "room_"+Math.ceil(Math.random()*10000);
+  //         return false;
+  //       }
+  //       if(room_id) return false;
+  //       // return true;
+  //     });
+
+  //     console.log("Room = "+room_id);
+  //     if(room_id!=0){// if opponent was found
+  //       //create new room
+  //       db.ref(room_id).set({
+  //         players: 0,
+  //         chat : ""
+  //       });
+  //       db.ref('waiting/' + opponent.id).set(room_id);
+  //       db.ref('waiting/'+ player.id).set(room_id);
+  //       //db.ref(room_id+'/players/'+player.id).set(player);
+  //     }
+  //     else{
+  //       db.ref('waiting/'+player.id).set(0);
+
+  //     }
+  //     query.off(); 
+  //   });
+  });
+}
+//=========== CHOICE CLICK ==============
 
 $("#player1").on("click", ".choice_btn", function(){
 
@@ -175,10 +219,8 @@ $("#player1").on("click", ".choice_btn", function(){
 
     if( opponent.choice && player.choice ){
 
-      console.log("p2-choice: "+ opponent.choice);
-      //if( player.choice ){
+      // console.log("p2-choice: "+ opponent.choice);
       var p2text = getTextForChoice( opponent.choice );
-      //draw/show pic for opponent choice
       $("#player2").text(p2text);
 
       //check who is the winner
@@ -201,10 +243,10 @@ $("#player1").on("click", ".choice_btn", function(){
       opponent.choice = 0;
       player.choice = 0;
 
-      //set self coice to 0 in db
-      db.ref(room_id+'/players/'+player.id+'/choice').set(0);
-      db.ref(room_id+'/players/'+player.id+'/wins').set(player.wins);
-      db.ref(room_id+'/players/'+player.id+'/looses').set(player.looses);
+      db.ref(room_id+'/players/'+player.id).set(player);
+      // db.ref(room_id+'/players/'+player.id+'/choice').set(0);
+      // db.ref(room_id+'/players/'+player.id+'/wins').set(player.wins);
+      // db.ref(room_id+'/players/'+player.id+'/looses').set(player.looses);
 
       setTimeout( drawMyChoiceButtons , SCORE_TIMEOUT );
       
@@ -216,6 +258,7 @@ $("#player1").on("click", ".choice_btn", function(){
 
 });//choice click
 
+// ==== START BUTTON ===============
 
 $("#btn-start").on("click", function(){
   event.preventDefault();
@@ -243,47 +286,43 @@ $("#btn-start").on("click", function(){
   db.ref('waiting/'+player.id).on('value', function(snapshot) {
 
       //get opponents id, add yourself to the room
-      console.log("waiting list changed : "+snapshot.val());
+      console.log("my waiting status changed : " + snapshot.val());
 
       if(snapshot.val()){
 
-        var player_obj = {};
-        player_obj[player.id] = player;
+        // var player_obj = {};
+        // player_obj[player.id] = player;
         room_id = snapshot.val();
-        db.ref(room_id+'/players').update(player_obj);
 
-        db.ref(room_id+'/players').on('value', function(snap){
-          console.log("getting players : "+snap.val());
-          if(snap.val()){
-            for(var p_id in snap.val()){
-              if(p_id != player.id ){
-                opponent = snap.val()[p_id];
-                break;
-                //todo: stop listening?
+        // db.ref(room_id+'/players').update(player_obj);
+        db.ref(room_id+'/players/'+player.id).set(player);
+
+        var query = db.ref(room_id+'/players');
+        query.on("value", function(parSnapshot) {
+          if(parSnapshot.exists() && parSnapshot.val()){
+            parSnapshot.forEach(function(chSnapshot) {
+              console.log("=players snap: "+chSnapshot.key);
+              var id = chSnapshot.key;
+              if( id != player.id ){
+                opponent = chSnapshot.val();
+                console.log("=players: opponent found "+chSnapshot.val());
+                
+                drawMyChoiceButtons();
+
+                //if player disconncted
+                var playerRef = db.ref(room_id+'/players/'+player.id);
+                playerRef.onDisconnect().remove();
+
+                //if opponent disconnected
+                processOpponentDisconnect();
+                
+                query.off();
+                return false;//stop looping
               }
-            }
-            drawPlayersInfo();
-
-            //if player disconncted
-            var playerRef = db.ref(room_id+'/players/'+player.id);
-            playerRef.onDisconnect().remove();
-
-            //if opponent disconnected
-            db.ref(room_id+'/players/'+opponent.id).on('child_removed', function(event){
-                  db.ref(room_id).remove();
-                  player.choice = 0;
-                  opponent = {};
-                  room_id = 0;
-                  clearButtons();
-                  //todo : check for other 0s in wroom
-                  //enterWaitList();
-                  db.ref('waiting/'+player.id).set(0);
-
+              if(opponent.id) return false;
             });
           }
         });
-
-        drawMyChoiceButtons();
       }
   }, 
     function(error){
